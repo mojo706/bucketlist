9736fbc3767c8f5b8e0425f1407609270b6ce4bc
chuchu <eugene.omar@andela.com>
2017-05-19 11:22:28 +0300

    Add items table and relationships

--- app/models.py
+++ app/models.py
@@ -1,7 +1,73 @@
-# app/models.py
 
-from app import db
+import os
+import datetime
+import jwt
+from flask import current_app
+from flask_bcrypt import Bcrypt
+from app import db, create_app
+from sqlalchemy.schema import ForeignKey
 
+# app = create_app(config_name=os.getenv('FLASK_CONFIG'))
+# bcrypt = Bcrypt(app)
+
+class User(db.Model):
+    """This class represents the user table."""
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    bucket = db.relationship(
+        'Bucketlist', cascade="all, delete-orphan")
+
+    # def __init__(self, email, password):
+    #     self.email = email
+    #     self.password = Bcrypt().generate_password_hash(password).decode()
+
+    def __init__(self, email, password, admin=False):
+        self.email = email
+        self.password = Bcrypt().generate_password_hash(
+            password).decode()
+        self.registered_on = datetime.datetime.now()
+
+    def encode_auth_token(self, email):
+        """
+        Generates the Auth Token
+        :return: string
+        """
+        try:
+            payload = {
+                'exp': datetime.datetime.now() + datetime.timedelta(days=0, seconds=5),
+                'iat': datetime.datetime.now(),
+                'sub': email
+            }
+            return jwt.encode(
+                payload,
+                current_app.config.get('SECRET_KEY'),
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        """
+        Decodes the auth token
+        :param auth_token:
+        :return: integer|string
+        """
+        try:
+            payload = jwt.decode(auth_token, current_app.config.get('SECRET_KEY'))
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
 class Bucketlist(db.Model):
     """This class represents the bucketlist table."""
@@ -14,6 +80,8 @@ class Bucketlist(db.Model):
     date_modified = db.Column(
         db.DateTime, default=db.func.current_timestamp(),
         onupdate=db.func.current_timestamp())
+    user_email = db.Column(db.String(255), db.ForeignKey(User.email))
+    items = db.relationship("Item", backref="bucketlists", passive_deletes=True)
 
     def __init__(self, name):
         """initialize with name."""
@@ -31,5 +99,21 @@ class Bucketlist(db.Model):
         db.session.delete(self)
         db.session.commit()
 
-    def __repr__(self):
-        return "<Bucketlist: {}>".format(self.name)
+    # def __repr__(self):
+    #     return "<Bucketlist: {}>".format(self.name)
+
+
+
+
+class Item(db.Model):
+    """This class represents the items table."""
+
+    __tablename__ = 'items'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(
+        db.DateTime, default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
+    done = db.Column(db.Boolean)
+    bucketlist = db.Column(db.Integer, db.ForeignKey("bucketlists.id", ondelete="CASCADE"))
