727c8eda19d9ffe0b54883a65a89e0dff354fbdf
chuchu <eugene.omar@andela.com>
2017-05-19 11:35:10 +0300

    Add method for registering a user

---
+++ app/auth/views.py
@@ -0,0 +1,75 @@
+
+from flask import request, json, route, jsonify, make_response
+from flask.views import MethodView
+from app import db
+from app.models import User
+from . import auth_blueprint
+
+class RegisterAPI(MethodView):
+    """
+    User Registration Resource
+    """
+
+    def post(self):
+        # get the post data
+        post_data = request.get_json()
+        # check if user already exists
+        user = User.query.filter_by(email=post_data.get('email')).first()
+        if not user:
+            try:
+                user = User(
+                    email=post_data.get('email'),
+                    password=post_data.get('password')
+                )
+
+                # insert the user
+                db.session.add(user)
+                db.session.commit()
+                # generate the auth token
+                auth_token = user.encode_auth_token(user.id)
+                responseObject = {
+                    'status': 'success',
+                    'message': 'Successfully registered.',
+                    'auth_token': auth_token.decode()
+                }
+                return make_response(jsonify(responseObject)), 201
+            except Exception as e:
+                responseObject = {
+                    'status': 'fail',
+                    'message': 'Some error occurred. Please try again.'
+                }
+                return make_response(jsonify(responseObject)), 401
+        else:
+            responseObject = {
+                'status': 'fail',
+                'message': 'User already exists. Please Log in.',
+            }
+            return make_response(jsonify(responseObject)), 202
+
+# define the API resources
+registration_view = RegisterAPI.as_view('register_api')
+login_view = LoginAPI.as_view('login_api')
+user_view = UserAPI.as_view('user_api')
+logout_view = LogoutAPI.as_view('logout_api')
+
+# add Rules for API Endpoints
+auth_blueprint.add_url_rule(
+    '/auth/register',
+    view_func=registration_view,
+    methods=['POST']
+)
+auth_blueprint.add_url_rule(
+    '/auth/login',
+    view_func=login_view,
+    methods=['POST']
+)
+auth_blueprint.add_url_rule(
+    '/auth/status',
+    view_func=user_view,
+    methods=['GET']
+)
+auth_blueprint.add_url_rule(
+    '/auth/logout',
+    view_func=logout_view,
+    methods=['POST']
+)
