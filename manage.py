483115dc4bfe85a9075685646386c8143c3af968
chuchu <eugene.omar@andela.com>
2017-05-19 11:30:04 +0300

    Add commands to create_db() and to drop_db()

--- manage.py
+++ manage.py
@@ -2,16 +2,28 @@
 
 import os
 
-from flask_script import Manager
+from flask_script import Manager, prompt_bool
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
 from app import models
 
-app = create_app(config_name=os.getenv('APP_SETTINGS'))
+app = create_app(config_name=os.getenv('FLASK_CONFIG'))
 migrate = Migrate(app, db)
 manager = Manager(app)
 
-manager.add_command('db', MigrateCommand)
+manager.add_command('db',MigrateCommand)
+
+@manager.command
+def initdb():
+    """ initialise the database with creation of tables """
+    db.create_all()
+    db.session.commit()
+
+@manager.command
+def dropdb():
+    """ Deletes database """
+    if prompt_bool("Are you sure you want to destroy all your data"):
+        db.drop_all()
 
 if __name__ == '__main__':
     manager.run()
