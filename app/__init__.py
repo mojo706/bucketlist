204b76cff02879c4dd9f74072eaac0dd6a119265
chuchu <eugene.omar@andela.com>
2017-05-19 11:21:46 +0300

    Add the blueprint for authentication

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,16 @@
 # app/__init__.py
 
+import os
+
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
+
 
 # local import
 from instance.config import app_config
 
 # initialize sql-alchemy
 db = SQLAlchemy()
-
 
 def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
