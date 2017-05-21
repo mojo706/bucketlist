fc362011cd272f58aff7db44179e8219ead9aa2d
chuchu <eugene.omar@andela.com>
2017-05-21 22:15:41 +0300

    Feature add test command to automate tests

--- manage.py
+++ manage.py
@@ -1,11 +1,13 @@
+#!/Users/chuchu/.virtualenvs/cp-bucketlist/bin/python3
+
 """ Keeps track of all the commands and handles how they are called from the command line. """
 
 import os
+import unittest
 
 from flask_script import Manager, prompt_bool
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
-from app import models
 
 app = create_app(config_name=os.getenv('FLASK_CONFIG'))
 migrate = Migrate(app, db)
@@ -24,6 +26,15 @@ def dropdb():
     """ Deletes database """
     if prompt_bool("Are you sure you want to destroy all your data"):
         db.drop_all()
+
+@manager.command
+def test():
+    """ Runs the unit tests for the bucketlist api """
+    tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 if __name__ == '__main__':
     manager.run()
