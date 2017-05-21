b4623b4b607abb21a32c910099d7d94a703fadbb
chuchu <eugene.omar@andela.com>
2017-05-21 19:35:40 +0300

    Add method for Creating, Viewing, Deleting and Updating a Bucketlist

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ import os
 
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
+from flask import request, jsonify, abort
 
 
 # local import
@@ -12,11 +13,87 @@ from instance.config import app_config
 # initialize sql-alchemy
 db = SQLAlchemy()
 
+
 def create_app(config_name):
+    """ Function that wraps the creation of a new Flask object, and returns it after it's loaded up with configuration settings """
+    from app.models import Bucketlist
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
+    @app.route('/api/v1.0/bucketlists/', methods=['POST', 'GET'])
+    def bucketlists():
+        """ Function that creates a new bucketlist or retrieves all bucketlists """
+        if request.method == "POST":
+            name = str(request.data.get('name', ''))
+            if name:
+                bucketlist = Bucketlist(name=name)
+                bucketlist.save()
+                response = jsonify({
+                    'id': bucketlist.id,
+                    'name': bucketlist.name,
+                    'date_created': bucketlist.date_created,
+                    'date_modified': bucketlist.date_modified,
+                    'created_by': bucketlist.created_by
+                })
+                response.status_code = 201
+                return response
+        else:
+            # GET
+            bucketlists = Bucketlist.get_all()
+            results = []
+
+            for bucketlist in bucketlists:
+                obj = {
+                    'id': bucketlist.id,
+                    'name': bucketlist.name,
+                    'date_created': bucketlist.date_created,
+                    'date_modified': bucketlist.date_modified
+                }
+                results.append(obj)
+            response = jsonify(results)
+            response.status_code = 200
+            return response
+
+    @app.route('/api/v1.0/bucketlists/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+    def update_bucketlist(id, **kwargs):
+        """ Function that depending on the HTTP Request will handle deleting, updating or getting a bucketlist respectively."""
+     # retrieve a buckelist using it's ID
+        bucketlist = Bucketlist.query.filter_by(id=id).first()
+        if not bucketlist:
+            # Raise an HTTPException with a 404 not found status code
+            abort(404)
+
+        if request.method == 'DELETE':
+            bucketlist.delete()
+            return {
+                "message": "bucketlist {} deleted successfully".format(bucketlist.id)
+            }, 200
+
+        elif request.method == 'PUT':
+            name = str(request.data.get('name', ''))
+            bucketlist.name = name
+            bucketlist.save()
+            response = jsonify({
+                'id': bucketlist.id,
+                'name': bucketlist.name,
+                'date_created': bucketlist.date_created,
+                'date_modified': bucketlist.date_modified
+            })
+            response.status_code = 200
+            return response
+        else:
+            # GET
+            response = jsonify({
+                'id': bucketlist.id,
+                'name': bucketlist.name,
+                'date_created': bucketlist.date_created,
+                'date_modified': bucketlist.date_modified
+            })
+            response.status_code = 200
+            return response
+
     return app
+
